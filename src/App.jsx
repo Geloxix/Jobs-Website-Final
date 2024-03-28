5dbe34d86c491f25c31ade790047f295fc6122c1
Marc Angelo <marcangelogilig1@gmail.com>
2024-03-28 22:51:31 +0800

    Version 5 Job Page finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import AddJobs from './pages/AddJobs';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, {jobLoader} from './pages/JobPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout />} >
             <Route index element={<HomePage />} />
             <Route path='/jobs' element={<JobsPage />} />
-            <Route path='/jobs/:id' element={<JobPage />} />
+            <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
             <Route path='/add-jobs' element={<AddJobs />} />
             <Route path='*' element={<NotFoundPage />} />
         </Route>
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,70 +1,108 @@
-import { useState, useEffect } from "react";
-import { FaMapMarkerAlt } from "react-icons/fa";
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import { useParams, useLoaderData, Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 const JobPage = () => {
     const { id } = useParams(); 
-    const [job, setJob] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const job = useLoaderData();
 
-    useEffect(() => {
-        
-        const fetchJob = async () => {
-            try {
-                const response = await fetch(`/api/jobs/${id}`);
-                const data = await response.json();
-                setJob(data);
-            } catch (err) {
-                console.log('Failed to fetch job data', err);
-            } finally {
-                setLoading(false);
-            }
-        }
+    return (
+        <>
+            <section>
+                <div className="container m-auto py-6 px-6">
+                    <Link
+                        to="/jobs"
+                        className="text-indigo-500 hover:text-indigo-600 flex items-center"
+                    >
+                        <FaArrowLeft className='mr-2'/> Back to Job Listings
+                    </Link>
+                </div>
+            </section>
 
-        fetchJob();
-    });
-    return loading ? <Spinner /> : (
-        <section className="bg-blue-50 px-4 py-10">
-            <div className="container-xl lg:container m-auto">
-                <div className='bg-white rounded-xl shadow-md relative'>
-                    <div className='p-4'>
-                        <div className='mb-6'>
-                            <div className='text-gray-600 my-2'>{job.type}</div>
-                            <h3 className='text-xl font-bold'>{job.title}</h3>
-                        </div>
-
-                        <div className='mb-2 font-palanquin'>
-                            {job.description}
-                        </div>
-
-                        <h3 className='text-indigo-500 mb-2'>
-                            {job.salary} / Year
-                        </h3>
-                        
-                        <div className='border border-gray-100 mb-5'></div>  
-
-                        <div className='flex flex-col lg:flex-row justify-between mb-2'>
-                            <div className='text-orange-700 mb-3'>
-                                <FaMapMarkerAlt className="inline text-lg mb-2 mr-2" />
-                                {job.location}
+            <section className="bg-indigo-50">
+                <div className="container m-auto py-10 px-6">
+                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                        <main>
+                            <div
+                                className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+                            >
+                                <div className="text-gray-500 mb-4">{ job.type }</div>
+                                <h1 className="text-3xl font-bold mb-4">
+                                    { job.title }
+                                </h1>
+                                <div
+                                    className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+                                >
+                                    <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                                    ></i>
+                                    <p className="text-orange-700">{ job.location }</p>
+                                </div>
                             </div>
+
+                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                            <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                                Job Description
+                            </h3>
+
+                            <p className="mb-4">
+                                { job.description }
+                            </p>
+
+                            <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+
+                            <p className="mb-4">{ `${job.salary } / Year` }</p>
+                            </div>
+                        </main>
+
+                    {/* <!-- Sidebar --> */}
+                    <aside>
+                        {/* <!-- Company Info --> */}
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <h3 className="text-xl font-bold mb-6">Company Info</h3>
+
+                            <h2 className="text-2xl">{ job.company.name }</h2>
+
+                            <p className="my-2">
+                                { job.company.description }
+                            </p>
+
+                            <hr className="my-4" />
+
+                            <h3 className="text-xl">Contact Email:</h3>
+
+                            <p className="my-2 bg-indigo-100 p-2 font-bold">
+                                { job.company.contactEmail }
+                            </p>
+
+                            <h3 className="text-xl">Contact Phone:</h3>
+
+                            <p className="my-2 bg-indigo-100 p-2 font-bold">{ job.company.contactPhone }</p>
                         </div>
 
-                        <div className="flex gap-3 flex-col font-palanquin">
-                            <h1 className="text-lg text-gray-600">Company</h1>
-                            <h2 className="font-bold mb-2 text-xl">{job.company.name} </h2>
-                            <div>{ job.company.description }</div>
-                            <div>
-                                <p className="underline text-red-500">{`Email: ${job.company.contactEmail}`}</p>
-                                <p className="">{`Contact: ${job.company.contactPhone}`}</p>
-                            </div>
+                        {/* <!-- Manage --> */}
+                        <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                            <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                            <Link
+                                to="/add-job"
+                                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                                >Edit Job</Link>
+                            <button
+                                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                            >
+                                Delete Job
+                            </button>
                         </div>
+                    </aside>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>  
     );
 }
 
-export default JobPage;
+const jobLoader = async({ params } ) => {
+    const response = await fetch(`/api/jobs/${params.id}`);
+    const data = await response.json();
+    return data;
+}
+
+export { JobPage as default, jobLoader};
